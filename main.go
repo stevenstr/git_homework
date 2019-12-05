ace84ce2220376cc68bb3327d88d0cd01701222d
Ste Zha <stephenzhavrid@gmail.com>
2019-12-05 14:50:06 +0300

    owner_name

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var hell = "Hello "
+	var name string
+	fmt.Println("please enter your name:")
+	fmt.Scanf("%s", &name)
+	fmt.Println(hell, name)
+	fmt.Println("How is the weather today?")
 }
